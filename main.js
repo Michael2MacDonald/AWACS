b4ff76ddef47e31bbbb61c881ec2927c5b91c11e
Michael MacDonald <michael2macdonald@gmail.com>
2020-11-08 08:29:03 -0800

    spamming and profanity filters

--- main.js
+++ main.js
@@ -6,6 +6,27 @@ const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 //const config = require('./config.json'); // uncomment this line
 const config = require('./config.private.json'); // comment out this line
 
+const AntiSpam = require('discord-anti-spam');
+const antiSpam = new AntiSpam({
+  warnThreshold: 4, // Amount of messages sent in a row that will cause a warning.
+  kickThreshold: 10, // Amount of messages sent in a row that will cause a kick.
+  banThreshold: 15, // Amount of messages sent in a row that will cause a ban.
+  maxInterval: 2500, // Amount of time (in milliseconds) in which messages are considered spam.
+  warnMessage: '{@user}, Please stop spamming.', // Message that will be sent in chat upon warning a user.
+  kickMessage: '*Scanning...*\n__***THREAT FOUND***__\n**{user_tag}** has been kicked for spamming!', // Message that will be sent in chat upon kicking a user.
+  banMessage: '*Scanning...*\n__***THREAT FOUND***__\n**{user_tag}** has been banned for spamming!', // Message that will be sent in chat upon banning a user.
+  maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
+  maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
+  maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
+  exemptPermissions: ['ADMINISTRATOR'], // Bypass users with any of these permissions.
+  ignoreBots: true, // Ignore bot messages.
+  verbose: true, // Extended Logs from module.
+  ignoredUsers: [], // Array of User IDs that get ignored.
+  // And many more options... See the documentation.
+});
+
+const profanity = require('./profanity.json');
+
 client.on('ready', () => {
   console.log("Connected as " + client.user.tag);
   client.user.setActivity("For Threats | Type AWACS help", {type: "WATCHING"});
@@ -24,6 +45,21 @@ client.on('message', (receivedMessage) => {
   //     return;
   //   }
   // }
+  antiSpam.message(receivedMessage);
+  if (receivedMessage.content) {
+    const profane = !!profanity.find((word) => {
+      const regex = new RegExp(`\\b${word}\\b`, 'i'); // if the phrase is not alphanumerical,
+      return regex.test(receivedMessage.content);             // you may need to escape tokens
+    });
+
+    if (profane) {
+      receivedMessage.channel.send("*Scanning...*");
+      receivedMessage.channel.send("__***THREAT FOUND***__\nTarget: <@" + receivedMessage.author.id + ">");
+      receivedMessage.delete().catch(console.error);
+      receivedMessage.channel.send("**Threat Eliminated!**");
+    }
+  }
+
   var messageSplit = receivedMessage.content.split(" "); // Split the message up in to pieces for each space
   messageSplit[0] = messageSplit[0].toLowerCase();
   if (messageSplit[0] == "awacs") { //check if message starts with awacs
@@ -32,7 +68,7 @@ client.on('message', (receivedMessage) => {
 })
 
 function processCommand(receivedMessage) { // gets the command and processes what needs to be done
-  var fullCommand = receivedMessage.content.substr(5); // Remove the leading exclamation mark
+  var fullCommand = receivedMessage.content.substr(6); // Remove the leading exclamation mark
   var splitCommand = fullCommand.split(" "); // Split the message up in to pieces for each space
   var primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
   var arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
@@ -50,7 +86,7 @@ function processCommand(receivedMessage) { // gets the command and processes wha
       pingCommand(arguments, receivedMessage);
     break;
     default:
-      receivedMessage.channel.send("I don't understand the command. Try `sudo help`, `sudo -h` or `sudo -h [command]`")
+      receivedMessage.channel.send("I don't understand the command. Try `awacs help`")
   }
 }
 
